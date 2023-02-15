6b3101cff786f0d5efececefa85fafa51cedf913
JigmeT123 <tashinamgyaljigme@gmail.com>
2023-02-16 01:39:57 +0400

    feat(): game intro section css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div className="game-container">
+				<div className="game-intro">
+					<h1>Rock - Paper - Scissors</h1>
+					<p>
+						Rock Paper Scissors is a zero sum game that is usually played by two
+						people using their hands and no tools. The idea is to make shapes
+						with an outstretched hand where each shape will have a certain
+						degree of power and will lead to an outcome.
+					</p>
+				</div>
+
+				<div className="choice-card"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
